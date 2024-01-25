b28230e15cc1191a0a95c20fb73e4704a5691786
WiTuLlo <98892685+WiTuLlo@users.noreply.github.com>
2024-01-25 18:14:06 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+  <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="generator" content="PSPad editor, www.pspad.com">
+  <title>Maturitní práce</title>
+  <meta content="Zapletal, Analýza volných míst na parkovišti pro parkování vozidel, Maturitní práce, Realizace hodin, Břeclav 2023, Střední škola Edvarda Beneše Břeclav, příspěvková organizace. Cílem mé maturitní práce je realizace sledování volných a obsazených parkovacích míst v programovacím jazyce Python s implementací umělé inteligence. Součástí práce je seznámení se s jazykem a prohloubení znalostí v programování. " name="description">
+  <meta content="Zapletal, Analýza volných míst na parkovišti pro parkování vozidel, Maturitní práce, Realizace hodin, Břeclav 2023, Střední škola Edvarda Beneše Břeclav, příspěvková organizace. Cílem mé maturitní práce je realizace sledování volných a obsazených parkovacích míst v programovacím jazyce Python s implementací umělé inteligence. Součástí práce je seznámení se s jazykem a prohloubení znalostí v programování. " name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/olo.png" rel="icon">
+  <link href="assets/img/olo.png" rel="apple-touch-icon">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  </head>
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <h1 class="logo"><a href="https://www.sseb.cz/">Maturitní projekt</a></h1>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Domů</a></li>
+          <li><a class="nav-link scrollto" href="#about">O projektu</a></li>
+          <li><a class="nav-link scrollto" href="#services">Způsoby realizace</a></li>
+          <li><a class="nav-link scrollto " href="#blog">Fotky projektu</a></li>
+          <li><a href="plakat2.html">Plakát projektu</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <div id="hero" class="hero route bg-image" style="background-image: url(assets/img/main.jpg)">
+    <div class="overlay-itro"></div>
+    <div class="hero-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <h1 class="hero-title mb-4">Analýza volných míst na parkovišti pro parkování vozidel</h1>
+          <p class="hero-subtitle"><span class="typed" data-typed-items="Ondřej Zapletal, Student SSEB"></span></p>
+        </div>
+      </div>
+    </div>
+  </div><!-- End Hero Section -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about-mf sect-pt4 route">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-5">
+                      <div class="about-img">
+                        <img src="assets/img/parkoviste1.png" class="img-fluid rounded b-shadow-a" alt="">
+                      </div>
+                    </div>
+                    <div class="col-sm-6 col-md-7">
+                      <div class="about-info">
+                        <p><span class="title-s">Projekt: </span> <span>Analýza volných míst na parkovišti pro parkování vozidel</span></p>
+                        <p><span class="title-s">Začátek projektu: </span> <span>1.11.2023</span></p>
+                        <p><span class="title-s">Termín odevzdání: </span> <span>29.3.2024</span></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="skill-mf">
+                  
+                    <p class="title-s">Použité kódy</p>
+                    <span>Python</span> <span class="pull-right">80%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>HTML</span> <span class="pull-right">15%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 15%;" aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>PHP</span> <span class="pull-right">5%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 5%" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="about-me pt-4 pt-md-0">
+                    <div class="title-box-2">
+                      <h5 class="title-left">
+                        O projektu
+                      </h5>
+                    </div>
+                    <p class="lead">
+                    Maturitní projekt se zaměřuje na programování v jazyce Python s využitím modelu YOLO pro objektové rozpoznávání k realizaci funkčního sledování parkovacích míst na konkrétním parkovišti. 
+                    </p>
+                    <p class="lead">
+                      <b>Pro realizaci projektu jsem použil:</b> Programovací jazyk Python, Moduly pro zpracování obrazu, jako je OpenCV, Moduly pro práci s databázemi, jako je PyMySQL, Model pro objektové rozpoznávání YOLO
+                    </p>
+                    <p class="lead">
+                     Program běží pravidelně, a kontroluje, zda jsou informace o obsazenosti parkovacích míst aktuální. Pokud jsou informace zastaralé, program je aktualizuje a uloží do databáze. Projekt je zajímavý a má potenciál zlepšit efektivitu dopravy a usnadnit hledání volných parkovacích míst.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services-mf pt-5 route">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="title-box text-center">
+              <h3 class="title-a">
+                Způsoby realizace
+              </h3>
+              <p class="subtitle-a">
+                Nejlevnější a nejlepší byla varianta přes kameru
+              </p>
+              <div class="line-mf"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+        
+          <div class="col-md-4">
+            <div class="service-box">
+              <div class="service-ico">
+                <img src="assets/img/kamera.png" style="float: center;" width="110" height="110">
+              </div>
+              <div class="service-content">
+                <h2 class="s-title">Kamerový systém</h2>
+                <p class="s-description text-center">
+                  Kamerový systém obvykle snímá obrázky parkovacích míst a používá algoritmy pro detekci vozidel. Mezi výhody patří detekce v jakémkoli prostředí, různých velikosti a tvarů. 
+                </p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col-md-4">
+            <div class="service-box">
+              <div class="service-ico">
+                <img src="assets/img/senzor.png" style="float: center;" width="110" height="110">
+              </div>
+              <div class="service-content">
+                <h2 class="s-title">Infračervené senzory</h2>
+                <p class="s-description text-center">
+                 Využívají infračervené záření k detekci vozidel. Infračervený senzor snímá infračervené záření, které odráží vozidla. Pro mě zbytečně nákladné na implementaci.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="service-box">
+              <div class="service-ico">
+                <img src="assets/img/tlak.png" style="float: center;" width="110" height="110">
+              </div>
+              <div class="service-content">
+                <h2 class="s-title">Senzory tlaku</h2>
+                <p class="s-description text-center">
+                 Měří tlak, který vyvíjejí vozidla na parkovacích místech. Senzor tlaku je umístěn pod parkovacím místem. Velmi odolné vůči povětrnostním podmínkám. 
+                </p>
+              </div>
+            </div>
+          </div>     
+          <center><div class="col-md-4">
+            <div class="service-box">
+              <div class="service-ico">
+                <img src="assets/img/indukce.png" style="float: center;" width="110" height="110">
+              </div>
+              <div class="service-content">
+                <h2 class="s-title">Senzory indukčnosti</h2>
+                <p class="s-description text-center">
+                 Využívají indukci k detekci vozidel. Senzor indukčnosti je obvykle umístěn pod parkovacím místem. Jsou použity pouze k detekci vozidel, která plně pokrývají parkovací místo.  
+                </p>
+              </div>
+            </div>
+          </div></center>
+        </div>
+      </div>
+    </section><!-- End Services Section -->
+    
+<section>
+<div class="section-counter paralax-mf bg-image" style="background-image: url(assets/img/parkovani.png)">
+<center>
+<b><h1 style="color: white;">Online stav parkoviště</h1></b>
+<table border="3" cellpadding = "8">
+<tr>
+<th style="color: white; font-size: 20px;">Datum a čas &nbsp;&nbsp;</th>
+<th style="color: green; font-size: 20px;">Volno &nbsp;&nbsp;</th>
+<th style="color: red; font-size: 20px;">Obsazeno</th>
+</tr>
+</center>
+     <!-- ======= Counter Section ======= -->
+     <?php
+     $db = new PDO("mysql:host=localhost;dbname=zapletal.el20", "zapletal.el20", "Ji2mpcF0");
+     $dotazText = "SELECT * FROM `parkoviste` ORDER BY datum DESC LIMIT 1";
+     $dotaz = $db->prepare($dotazText); // příprava dotazu k odeslání do databáze
+     $dotaz->execute();   // odesíláme dotaz na server MySQL
+      while ($radek = $dotaz->fetch()) {    // metoda fetch() vrací jeden záznam 
+      echo "<TR>";
+      echo "<center>"."<TD style='font-size: 19px;'>"."<font color=white>"."<b>".$radek['datum']."</td>"."<br>";
+      echo "<TD style='font-size: 19px;'>"."<font color=white>"."<b>".$radek['volno']."</td>"."<br>";
+      echo "<TD style='font-size: 19px;'>"."<font color=white>"."<b>".$radek['obsazeno']."</td>";
+      echo "</TR>"."<center>";
+     }
+     $dotaz->closeCursor(); // uvolnění spojení mezi PHP a MySQL
+    ?>
+    </table>
+</div>
+ </section>   
+    <!-- ======= Blog Section ======= -->
+    <section id="blog" class="blog-mf sect-pt4 route">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="title-box text-center">
+              <h3 class="title-a">
+                Fotky projektu
+              </h3>
+              <p class="subtitle-a">
+                Následujicí fotky jsou z mého projektu, včetně malé části kódu
+              </p>
+              <div class="line-mf"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card card-blog">
+              <div class="card-img">
+               <img src="assets/img/parkoviste2.png" alt="" class="img-fluid">
+              </div>
+              <div class="card-body">
+                <div class="card-category-box">
+                  <div class="card-category">
+                    <h6 class="category">Program</h6>
+                  </div>
+                </div>
+                <h3 class="card-title">Ukázka programu</h3>
+                <p class="card-description">
+                  Nyní můžete vidět okno, které se otevře při spuštění mého programu v Pythonu. Automaticky rozpozná obsazenost parkoviště a vypíše hodnoty v reálném čase.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-blog">
+              <div class="card-img">
+                <img src="assets/img/kod1.png" alt="" class="img-fluid">
+              </div>
+              <div class="card-body">
+                <div class="card-category-box">
+                  <div class="card-category">
+                    <h6 class="category">Kód</h6>
+                  </div>
+                </div>
+                <h3 class="card-title">Část mého kódu</h3>
+                <p class="card-description">
+                  Tahle část kódu má nastarosti zpracování snímku pomocí modelu YOLO a vykreslení výsledků na snímku. Více detailně je popsané v mojí maturitní práci.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-blog">
+              <div class="card-img">
+               <img src="assets/img/tab.png" alt="" class="img-fluid">
+              </div>
+              <div class="card-body">
+                <div class="card-category-box">
+                  <div class="card-category">
+                    <h6 class="category">PHPMyAdmin</h6>
+                  </div>
+                </div>
+                <h3 class="card-title">Ukázka tabulky</h3>
+                <p class="card-description">
+                  Program vypisuje v reálném čase informace o obsazenosti parkovišti do tabulky. Použil jsem databázy PHPMyAdmin a program byl nastaven na zapisování každých 10 sekund.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section><!-- End Blog Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="copyright-box">
+            <p class="copyright">&copy; Copyright <strong>Zapletal</strong>. All Rights Reserved</p>
+            <div class="credits">
+              
+              Designed by <a href="https://www.sseb.cz/">Ondřej Zapletal</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
